827dedc4dd32beb7ace4b59b665b27a1d1795825
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-06-02 20:42:49 -0500

    MVP donegit add .git add .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import Card from "./components/Card";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: "",
+      followers: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://api.github.com/users/VickieNelson").then((res1) => {
+      console.log(res1.data);
+      axios.get(res1.data.followers_url).then((res2) => {
+        console.log(res2.data);
+        this.setState({
+          user: res1.data,
+          followers: res2.data,
+        });
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className='large-div'>
+        <h1>Github Usercards</h1>
+        <div>
+          {/* //card content */}
+          <Card
+            userImg={this.state.user.avatar_url}
+            name={this.state.user.name}
+            username={this.state.user.login}
+            profile={this.state.user.url}
+            followers={this.state.user.followers}
+            following={this.state.user.following}
+            bio={this.state.user.bio}
+          />
+        </div>
+
+        <h2>Followers</h2>
+        {/* //follower card content */}
+        {this.state.followers.map((user) => {
+          return (
+            <Card
+              userImg={user.avatar_url}
+              name={user.name}
+              username={user.login}
+              profile={user.url}
+              followers={user.followers}
+              following={user.following}
+              bio={user.bio}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
-
 export default App;
